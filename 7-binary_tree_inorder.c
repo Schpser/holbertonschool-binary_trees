588ec77d5b16f94f7e90a7d62fdaea80456ba24e
Schpser <schps.games@gmail.com>
2025-08-04 18:51:12 +0200

    Going trough binary tree (In-Order & Post-Order)

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder_reverse - Going trough a tree, In-order Traversal
+ * @tree: Pointer to the root
+ * @func: Pointer to the function
+ *
+ * Return: 1 if the node is a root, 0 otherwise (including if node is NULL)
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder_reverse - Going trough a tree, Post-order Traversal
+ * @tree: Pointer to the root
+ * @func: Pointer to the function
+ *
+ * Return: 1 if the node is a root, 0 otherwise (including if node is NULL)
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
