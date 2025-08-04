6a6ecd67981b871b683e4df2de905c396a95dac0
Schpser <schps.games@gmail.com>
2025-08-04 17:01:45 +0200

    Fix Add Node For Is Leaf Output

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,11 +19,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent->right != NULL)
 	{
-		new_node->right = parent->left;
-		parent->right = parent->left;
+		new_node->right = parent->right;
+		parent->right = parent->right;
 	}
 
-	parent->left = new_node;
+	parent->right = new_node;
 
 	return (new_node);
 }
