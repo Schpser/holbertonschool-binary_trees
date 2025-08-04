58c3fc2109d722837e2542ddf06a9b8782c46f3d
Schpser <schps.games@gmail.com>
2025-08-04 22:54:51 +0200

    Calculate size/height of a tree, depth of a node

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Measures the depth of a node
+ * @tree: Pointer to the node to measure
+ *
+ * Return: Depth of the node (0 if tree is NULL or root)
+ *
+ * Description:
+ * - La profondeur = nombre de liens entre le nœud et la racine.
+ * - Si le nœud est NULL ou la racine, retourne 0.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
