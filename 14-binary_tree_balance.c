be4763c9afb473e45a48994658559e74e0c4d8ce
Schpser <schps.games@gmail.com>
2025-08-06 12:46:38 +0200

    Add 9-binary_tree_height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	
+
 	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
@@ -34,8 +34,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	height_left = tree->left ? (int)binary_tree_height(tree->left) +1 : 0;
-	height_right = tree->right ? (int)binary_tree_height(tree->right) +1 : 0;
+	height_left = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
+	height_right = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
 
 	return (height_left - height_right);
 }
