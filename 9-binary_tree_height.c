5beace473f1a9ec0258985ca793f37f282ffda32
Schpser <schps.games@gmail.com>
2025-08-06 14:14:08 +0200

    Adapted from task 14

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t h_left, h_right;
+	size_t h_left = 0, h_right = 0;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	
-    h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return ((int)h_left - (int)h_right);
+	h_left = 1 + binary_tree_height(tree->left);
+	h_right = 1 + binary_tree_height(tree->right);
+
+	return (h_left > h_right ? h_left : h_right);
 }
+
