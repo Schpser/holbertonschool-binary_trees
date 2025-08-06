dc644e80d45351bb9ee76887e2fc8b1931820b08
Schpser <schps.games@gmail.com>
2025-08-06 15:02:34 +0200

    Check uncle (parents siblings)

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - Finds the sibling of a node in a binary tree.
- * @node: A pointer to the node to find the sibling of.
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: Pointer to the node to find the sibling of.
  *
  * Return: A pointer to the sibling node, or NULL if no sibling exists.
  */
