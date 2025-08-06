870659896e5ffbddc4f32316485dafa750acbfa9
Schpser <schps.games@gmail.com>
2025-08-06 14:13:40 +0200

    Correct outputs

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,15 +9,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t h_left, h_right;
+	size_t h_left = 0, h_right = 0;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	h_left = 1 + binary_tree_height(tree->left);
+	h_right = 1 + binary_tree_height(tree->right);
 
-	return ((int)h_left - (int)h_right);
+	return (h_left > h_right ? h_left : h_right);
 }
 
 /**
@@ -29,13 +29,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_left, height_right;
+	int h_left, h_right;
 
 	if (!tree)
 		return (0);
 
-	height_left = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
-	height_right = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
+	h_left = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
+	h_right = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
 
-	return (height_left - height_right);
+	return (h_left - h_right);
 }
