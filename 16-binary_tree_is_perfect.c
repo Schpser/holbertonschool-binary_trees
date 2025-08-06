29c98103173eaaecc7adc5046f6f1cb539dbad43
Schpser <schps.games@gmail.com>
2025-08-06 14:20:03 +0200

    Check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,23 +9,18 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_depth, right_depth;
-	int left_perfect, right_perfect;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
-	left_depth = binary_tree_depth(tree->left);
-	right_depth = binary_tree_depth(tree->right);
+	if (tree->left > tree->right)
+		return (tree->left - tree->right);
 
-	if (!tree->left && !tree->right)
+	if (tree->right > tree->left)
+		return (tree->right - tree->left);
+
+	if (tree->left == tree->right)
 		return (1);
 
-	if (left_depth != right_depth)
-		return (0);
-
-	left_perfect = binary_tree_is_perfect(tree->left);
-	right_perfect = binary_tree_is_perfect(tree->right);
-
-	return (left_perfect * right_perfect);
+	return (0);
 }
