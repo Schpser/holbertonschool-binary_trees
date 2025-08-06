29634e37ebce7267a142f156f9f41e692d465c13
Schpser <schps.games@gmail.com>
2025-08-06 12:45:40 +0200

    Add 9-binary_tree_height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,26 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Calculate tree'es height
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: 0 if NULL, tree's height otherwise
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left, h_right;
+
+	if (!tree)
+		return (0);
+	
+	h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	h_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return ((int)h_left - (int)h_right);
+}
+
+/**
  * binary_tree_balance - Calculate tree's balance
  * @tree: Pointer to the root
  *
@@ -14,8 +34,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	height_left = tree->left ? (int)binary_tree_height(tree->left) : -1;
-	height_right = tree->right ? (int)binary_tree_height(tree->right) : -1;
+	height_left = tree->left ? (int)binary_tree_height(tree->left) +1 : 0;
+	height_right = tree->right ? (int)binary_tree_height(tree->right) +1 : 0;
 
 	return (height_left - height_right);
 }
